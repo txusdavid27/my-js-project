72a611ecd3f4fb29b2cc1d2f442c22b00502c962
jesus-traslavina_xpo <jesus.traslavina@xpo.com>
2024-12-25 21:08:37 -0500

    TAE

--- test/firstTest.spec.js
+++ test/firstTest.spec.js
@@ -1,9 +1,26 @@
+const express = require('express')
+const request = require('supertest')
+const expect = require('chai').expect
 
-const chai = require('chai');
-const expect = chai.expect;
+const app = express();
 
-describe('Sample Test', () => {
-  it('should pass', () => {
-    expect(true).to.be.true;
+app.get('/first', (err, res) => {
+  res.status(200).json({'OK':"response"});
+});
+
+describe('First test', () => {
+  it('OK response', () => {
+    request(app)
+      .get('/first')
+      .end((err, res) => {
+        expect(res.statusCode).to.be.equal(200);
+      });
   });
+
+  it('Mocky OK response', (done) => {
+    request('http://www.mocky.io')
+      .get('/v3/cb1fe20a-8944-42fc-924e-be92d2f294c6')
+      .expect(200,done);
+  });
+
 });
