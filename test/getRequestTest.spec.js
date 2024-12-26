fb2f1dfa0af8458e4dcd9359349e038373f7c578
jesus-traslavina_xpo <jesus.traslavina@xpo.com>
2024-12-26 10:42:09 -0500

    step 2

---
+++ test/getRequestTest.spec.js
@@ -0,0 +1,20 @@
+const app = require('../src/app');
+const request = require('supertest');
+const expect = require('chai').expect;
+
+describe('get requests', () => {
+    it('get course id', () => {
+        request(app)
+        .get('/course/1')
+        .end((err, res) => {
+            expect(res.body.id).to.be.equal('1');
+        });
+    });
+    // localhost/course?name=mocha
+    it('get query param name',(done) => {
+        request(app)
+        .get('/course')
+        .query({'name':'mocha'})
+        .expect(200, { id: '1', name: 'mocha' }, done );
+    });
+});
